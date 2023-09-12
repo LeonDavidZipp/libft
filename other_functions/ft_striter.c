5949937ca073564b7f52f6024686545df5a8ab2e
Leon David Zipp <leondavidzipp@gmx.de>
2023-09-12 11:27:04 +0200

    started function functions

---
+++ other_functions/ft_striter.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leon <leon@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 11:11:47 by leon              #+#    #+#             */
+/*   Updated: 2023/09/12 11:26:39 by leon             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_striter(char *s, void (*f)(char *))
+{
+	if (s && f)
+	{
+		while (*s)
+		{
+			f(s);
+			s++;
+		}
+	}
+}
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// int	ft_tolower(int c)
+// {
+// 	if (c >= 65 && c <= 90)
+// 		c += 32;
+// 	return (c);
+// }
+// void	ft_tolower_wrapper(char *c)
+// {
+//     *c = ft_tolower(*c);
+// }
+// int	main(void)
+// {
+//     char str1[] = "Hello, World!";
+//     char str2[] = "12345";
+//     char str4[] = "   ";
+//     char str5[] = "NoSpacesHere";
+//     ft_striter(str1, &ft_tolower_wrapper);
+//     ft_striter(str2, &ft_tolower_wrapper);
+//     ft_striter(str4, &ft_tolower_wrapper);
+//     ft_striter(str5, &ft_tolower_wrapper);
+//     printf("ft_striter: expected output: \"hello, world!\" | \"%s\"\n", str1);
+//     printf("ft_striter: expected output: \"12345\" | \"%s\"\n", str2);
+//     printf("ft_striter: expected output: \"   \" | \"%s\"\n", str4);
+//     printf("ft_striter: expected output: \"nospaceshere\" | \"%s\"\n", str5);
+//     return (0);
+// }
