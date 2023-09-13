d44d9fffa8e1dad261bcddf2b95f63543b434393
Leon David Zipp <leondavidzipp@gmx.de>
2023-09-13 20:48:05 +0200

    removed wrong pushes

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: leon <leon@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 15:21:49 by leon              #+#    #+#             */
-/*   Updated: 2023/09/13 20:34:37 by leon             ###   ########.fr       */
+/*   Updated: 2023/09/13 20:47:57 by leon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,25 +38,25 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 	new->next = NULL;
 	return (new);
 }
-#include <stdio.h>
-int main(void)
-{
-    char *str = "Hello, World!";
-    t_list *node1, *node2;
-    node1 = ft_lstnew(str, strlen(str) + 1);
-    if (node1)
-    {
-        printf("Node1 Content: %s\n", (char *)node1->content);
-        printf("Node1 Content Size: %zu\n", node1->content_size);
-        free(node1->content);
-        free(node1);
-    }
-    node2 = ft_lstnew(NULL, 0);
-    if (node2)
-    {
-        printf("Node2 Content: %p\n", node2->content);
-        printf("Node2 Content Size: %zu\n", node2->content_size);
-        free(node2);
-    }
-    return (0);
-}
+// #include <stdio.h>
+// int main(void)
+// {
+//     char *str = "Hello, World!";
+//     t_list *node1, *node2;
+//     node1 = ft_lstnew(str, strlen(str) + 1);
+//     if (node1)
+//     {
+//         printf("Node1 Content: %s\n", (char *)node1->content);
+//         printf("Node1 Content Size: %zu\n", node1->content_size);
+//         free(node1->content);
+//         free(node1);
+//     }
+//     node2 = ft_lstnew(NULL, 0);
+//     if (node2)
+//     {
+//         printf("Node2 Content: %p\n", node2->content);
+//         printf("Node2 Content Size: %zu\n", node2->content_size);
+//         free(node2);
+//     }
+//     return (0);
+// }
