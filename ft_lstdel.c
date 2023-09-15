3b083df383ecbcbf6cda2eaca2524f172bb00357
Leon David Zipp <leondavidzipp@gmx.de>
2023-09-15 11:16:59 +0200

    removed ft_lstmap, too difficult atm

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: leon <leon@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 20:59:54 by leon              #+#    #+#             */
-/*   Updated: 2023/09/13 21:17:26 by leon             ###   ########.fr       */
+/*   Updated: 2023/09/15 11:02:54 by leon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,3 +28,31 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 		*alst = NULL;
 	}
 }
+// #include <stdio.h>
+// void	del(void *content, size_t content_size)
+// {
+//     if (content)
+//     {
+//         ft_bzero(content, content_size);
+//         free(content);
+//     }
+// }
+// int	main(void)
+// {
+//     t_list	*lst;
+//     t_list	*elem1;
+//     t_list	*elem2;
+//     t_list	*elem3;
+//     elem1 = ft_lstnew("Hello", 6);
+//     elem2 = ft_lstnew("World", 6);
+//     elem3 = ft_lstnew("!", 2);
+//     elem1->next = elem2;
+//     elem2->next = elem3;
+//     lst = elem1;
+//     ft_lstdel(&lst, &del);
+//     if (lst == NULL)
+//         printf("All elements deleted successfully\n");
+//     else
+//         printf("Error: elements still present in the list\n");
+//     return (0);
+// }
