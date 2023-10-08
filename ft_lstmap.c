d0167794defaf41513877c058cc7234752ace351
Leon David Zipp <leondavidzipp@gmx.de>
2023-10-08 13:33:57 +0200

    adjusted makefile

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:31:00 by lzipp             #+#    #+#             */
-/*   Updated: 2023/10/08 11:31:03 by lzipp            ###   ########.fr       */
+/*   Updated: 2023/10/08 12:01:13 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,18 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new;
+	t_list	*new_lst;
+	t_list	*new_elem;
 
+	new_lst = NULL;
 	while (lst != NULL)
 	{
-		new = (t_list *)malloc(sizeof(t_list));
-		if (!new)
+		new_elem = (t_list *)malloc(sizeof(t_list));
+		if (!new_elem)
 		{
-			;
+			ft_lstclear(&new_lst, del);
+			return(NULL);
 		}
+		new_elem = f(lst);
 	}
 }
