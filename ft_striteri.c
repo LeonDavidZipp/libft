a5e72e1c14443c8e0f5768191394575dab93475d
Leon David Zipp <lzipp@2-I-4.42heilbronn.de>
2023-10-09 16:38:43 +0200

    nothing special

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:32:42 by lzipp             #+#    #+#             */
-/*   Updated: 2023/10/09 16:37:51 by lzipp            ###   ########.fr       */
+/*   Updated: 2023/10/09 16:38:35 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,28 +21,3 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 		i++;
 	}
 }
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// void	ft_toupper_wrapper(unsigned int i, char *c);
-// int	main(void)
-// {
-//     char str1[] = "Hello, World!";
-//     char str2[] = "12345";
-//     char str4[] = "   ";
-//     char str5[] = "NoSpacesHere";
-//     ft_striteri(str1, &ft_toupper_wrapper);
-//     ft_striteri(str2, &ft_toupper_wrapper);
-//     ft_striteri(str4, &ft_toupper_wrapper);
-//     ft_striteri(str5, &ft_toupper_wrapper);
-//     printf("expected output: \"HELLO, WORLD!\" | \"%s\"\n", str1);
-//     printf("expected output: \"12345\" | \"%s\"\n", str2);
-//     printf("expected output: \"   \" | \"%s\"\n", str4);
-//     printf("expected output: \"NOSPACESHERE\" | \"%s\"\n", str5);
-//     return (0);
-// }
-// void	ft_toupper_wrapper(unsigned int i, char *c)
-// {
-//     if (c[i] >= 'a' && c[i] <= 'z')
-//         c[i] -= 32;
-// }
