8e3219a2008102bfb280a85fcb622f2fe37b2e18
Leon David Zipp <lzipp@2-I-4.42heilbronn.de>
2023-10-09 16:33:40 +0200

    nothing special

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,58 +6,35 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:31:44 by lzipp             #+#    #+#             */
-/*   Updated: 2023/10/09 14:49:02 by lzipp            ###   ########.fr       */
+/*   Updated: 2023/10/09 16:33:34 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	greater(unsigned char *dst, unsigned char *src, size_t len)
-{
-	size_t	i;
-	char	next;
-
-	i = 0;
-	while (i < len)
-	{
-		if (i + 1 < len)
-			next = dst[i + 1];
-		else
-			next = dst[i];
-		dst[i] = src[i];
-		i++;
-	}
-}
-
-static void	less(unsigned char *dst, unsigned char *src, size_t len)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < len)
-	{
-		dst[i] = src[i];
-		i++;
-	}
-}
-
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char	*dest;
 	unsigned char	*source;
+	size_t			i;
 
 	dest = (unsigned char *)dst;
 	source = (unsigned char *)src;
 	if (dst > src)
-		greater(dest, source, len);
+	{
+		while (len-- > 0)
+		{
+			dest[len] = source[len];
+		}
+	}
 	else if (dst < src)
-		less(dest, source, len);
+	{
+		i = 0;
+		while (i < len)
+		{
+			dest[i] = source[i];
+			i++;
+		}
+	}
 	return ((void *)dst);
 }
-
-// int	main(void)
-// {
-// 	char sResult[] = {67, 68, 67, 68, 69, 0, 45};
-// 	ft_memmove(sResult + 1, sResult, 2);
-// 	return (0);
-// }
