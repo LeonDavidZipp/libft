1cdc06c12c31157fb15865ab9de2609c1b590a30
Leon David Zipp <lzipp@2-H-6.42heilbronn.de>
2023-10-10 09:55:29 +0200

    nothing special

--- ft_itoa.c
+++ ft_itoa.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lzipp <lzipp@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:30:17 by lzipp             #+#    #+#             */
-/*   Updated: 2023/10/08 11:30:19 by lzipp            ###   ########.fr       */
+/*   Updated: 2023/10/10 09:54:51 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,11 @@ char	*ft_itoa(int n)
 	long	nb;
 	int		len;
 	char	*result;
+	int		start;
 
 	nb = (long) n;
 	len = ft_numlen(nb) + 1;
+	start = (nb < 0);
 	if (nb < 0)
 		nb *= -1;
 	result = (char *)malloc(len * sizeof(char));
@@ -49,37 +51,37 @@ char	*ft_itoa(int n)
 		result[0] = '-';
 	result[len - 1] = '\0';
 	len -= 2;
-	while (len >= 0 && result[len] != '-')
+	while (len >= start)
 	{
 		result[len--] = (nb % 10) + '0';
 		nb /= 10;
 	}
 	return (result);
 }
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// int	main(void)
-// {
-//     int num1 = 12345 - 300;
-//     int num2 = -6789;
-//     int num3 = 0;
-//     int num4 = -2147483648;
-//     int num5 = 2147483647;
-//     char *result1 = ft_itoa(num1);
-//     char *result2 = ft_itoa(num2);
-//     char *result3 = ft_itoa(num3);
-//     char *result4 = ft_itoa(num4);
-//     char *result5 = ft_itoa(num5);
-//     printf("ft_itoa: expected output: \"12345\" | \"%s\"\n", result1);
-//     printf("ft_itoa: expected output: \"-6789\" | \"%s\"\n", result2);
-//     printf("ft_itoa: expected output: \"0\" | \"%s\"\n", result3);
-//     printf("ft_itoa: expected output: \"-2147483648\" | \"%s\"\n", result4);
-//     printf("ft_itoa: expected output: \"2147483647\" | \"%s\"\n", result5);
-//     free(result1);
-//     free(result2);
-//     free(result3);
-//     free(result4);
-//     free(result5);
-//     return (0);
-// }
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int	main(void)
+{
+    int num1 = 12345 - 300;
+    int num2 = -1;
+    int num3 = 0;
+    int num4 = -2147483648;
+    int num5 = 2147483647;
+    char *result1 = ft_itoa(num1);
+    char *result2 = ft_itoa(num2);
+    char *result3 = ft_itoa(num3);
+    char *result4 = ft_itoa(num4);
+    char *result5 = ft_itoa(num5);
+    printf("ft_itoa: expected output: \"12045\" | \"%s\"\n", result1);
+    printf("ft_itoa: expected output: \"-6789\" | \"%s\"\n", result2);
+    printf("ft_itoa: expected output: \"0\" | \"%s\"\n", result3);
+    printf("ft_itoa: expected output: \"-2147483648\" | \"%s\"\n", result4);
+    printf("ft_itoa: expected output: \"2147483647\" | \"%s\"\n", result5);
+    free(result1);
+    free(result2);
+    free(result3);
+    free(result4);
+    free(result5);
+    return (0);
+}
