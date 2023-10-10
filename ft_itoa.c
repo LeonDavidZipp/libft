759a72fb6c8315d1e3b3216ea2ba8e47700002f4
Leon David Zipp <lzipp@2-F-8.42heilbronn.de>
2023-10-10 11:21:05 +0200

    nothing special

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:30:17 by lzipp             #+#    #+#             */
-/*   Updated: 2023/10/10 09:54:51 by lzipp            ###   ########.fr       */
+/*   Updated: 2023/10/10 11:09:09 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,30 +58,30 @@ char	*ft_itoa(int n)
 	}
 	return (result);
 }
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-int	main(void)
-{
-    int num1 = 12345 - 300;
-    int num2 = -1;
-    int num3 = 0;
-    int num4 = -2147483648;
-    int num5 = 2147483647;
-    char *result1 = ft_itoa(num1);
-    char *result2 = ft_itoa(num2);
-    char *result3 = ft_itoa(num3);
-    char *result4 = ft_itoa(num4);
-    char *result5 = ft_itoa(num5);
-    printf("ft_itoa: expected output: \"12045\" | \"%s\"\n", result1);
-    printf("ft_itoa: expected output: \"-6789\" | \"%s\"\n", result2);
-    printf("ft_itoa: expected output: \"0\" | \"%s\"\n", result3);
-    printf("ft_itoa: expected output: \"-2147483648\" | \"%s\"\n", result4);
-    printf("ft_itoa: expected output: \"2147483647\" | \"%s\"\n", result5);
-    free(result1);
-    free(result2);
-    free(result3);
-    free(result4);
-    free(result5);
-    return (0);
-}
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// int	main(void)
+// {
+//     int num1 = 12345 - 300;
+//     int num2 = -1;
+//     int num3 = 0;
+//     int num4 = -2147483648;
+//     int num5 = 2147483647;
+//     char *result1 = ft_itoa(num1);
+//     char *result2 = ft_itoa(num2);
+//     char *result3 = ft_itoa(num3);
+//     char *result4 = ft_itoa(num4);
+//     char *result5 = ft_itoa(num5);
+//     printf("ft_itoa: expected output: \"12045\" | \"%s\"\n", result1);
+//     printf("ft_itoa: expected output: \"-6789\" | \"%s\"\n", result2);
+//     printf("ft_itoa: expected output: \"0\" | \"%s\"\n", result3);
+//     printf("ft_itoa: expected output: \"-2147483648\" | \"%s\"\n", result4);
+//     printf("ft_itoa: expected output: \"2147483647\" | \"%s\"\n", result5);
+//     free(result1);
+//     free(result2);
+//     free(result3);
+//     free(result4);
+//     free(result5);
+//     return (0);
+// }
--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -1,31 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lzipp <lzipp@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:31:00 by lzipp             #+#    #+#             */
-/*   Updated: 2023/10/08 13:35:24 by lzipp            ###   ########.fr       */
+/*   Updated: 2023/10/10 11:20:50 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-// {
-// 	t_list	*new_lst;
-// 	t_list	*new_elem;
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_lst;
+	t_list	*new_elem;
 
-// 	new_lst = NULL;
-// 	while (lst != NULL)
-// 	{
-// 		new_elem = (t_list *)malloc(sizeof(t_list));
-// 		if (!new_elem)
-// 		{
-// 			ft_lstclear(&new_lst, del);
-// 			return(NULL);
-// 		}
-// 		new_elem = f(lst);
-// 	}
-// }
+	new_lst = NULL;
+	while (lst != NULL)
+	{
+		new_elem = (t_list *)malloc(sizeof(t_list));
+		if (!new_elem)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		new_elem = f(lst);
+	}
+	return (new_lst);
+}
