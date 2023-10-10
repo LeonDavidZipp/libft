76a94fd1771bd2fef4abcdfb73c6ee6a7ec2e5a9
LeondavidZipp <leondavidzipp@gmx.de>
2023-10-10 14:08:34 +0200

    nothing special

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 23:01:08 by lzipp             #+#    #+#             */
-/*   Updated: 2023/10/10 11:22:35 by lzipp            ###   ########.fr       */
+/*   Updated: 2023/10/10 14:06:56 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,56 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		lst = lst->next;
 	}
 	return (new_lst);
+}
+// system("leaks a.out")
+
+// Wrapper function for ft_lstclear
+void *wrapper_lstclear(void *content)
+{
+    t_list **lst = (t_list **)content;
+    ft_lstclear(lst, del_function); // Replace 'del_function' with your actual del function
+    return NULL; // This function doesn't return any value
+}
+
+// Wrapper function for ft_strtrim
+void *wrapper_strtrim(void *content)
+{
+    char **str = (char **)content;
+    char *trimmed = ft_strtrim(*str, "Ele 123"); // Replace 'set' with your actual set string
+    return (void *)trimmed; // Cast the result back to void*
+}
+
+
+int main() {
+    // Create three elements for the linked list
+    t_list *element1 = (t_list *)malloc(sizeof(t_list));
+    t_list *element2 = (t_list *)malloc(sizeof(t_list));
+    t_list *element3 = (t_list *)malloc(sizeof(t_list));
+
+    // Set content for each element
+    element1->content = "Element 1";
+    element2->content = "Element 2";
+    element3->content = "Element 3";
+
+    // Link the elements together to form the list
+    element1->next = element2;
+    element2->next = element3;
+    element3->next = NULL; // The last element's 'next' should be NULL
+
+    // Now, 'element1' is the head of the linked list
+
+    // Create a list of wrapper functions
+    t_list *wrapper_list = NULL;
+    ft_lstadd_back(&wrapper_list, ft_lstnew(&element1));
+    ft_lstadd_back(&wrapper_list, ft_lstnew(&element2));
+    ft_lstadd_back(&wrapper_list, ft_lstnew(&element3));
+
+    // Call ft_lstmap with wrapper functions
+    t_list *result = ft_lstmap(wrapper_list, wrapper_strtrim, wrapper_lstclear);
+
+    // Clean up memory (free allocated elements)
+    // You need to implement proper memory cleanup for result and wrapper_list
+    // based on the actual structure of your code
+
+    return 0;
 }
--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:32:23 by lzipp             #+#    #+#             */
-/*   Updated: 2023/10/10 11:40:30 by lzipp            ###   ########.fr       */
+/*   Updated: 2023/10/10 13:56:18 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,41 +38,19 @@ static int	ft_get_wlen(const char *s, char c)
 	return (len);
 }
 
-static void	ft_free_mem(char **result, int j)
+static int	ft_free_mem(char **result, int j)
 {
-	while (j >= 0)
+	if (!result[j])
 	{
-		free(result[j]);
-		j--;
-	}
-	free(*result);
-}
-
-static char	**ft_mini_split(const char *s, char c, int i, int j, int k)
-{
-	char	**result;
-
-	result = (char **)malloc((ft_count_words(s, c) + 1) * sizeof(char *));
-	if (!result)
-		return (NULL);
-	i = 0;
-	j = 0;
-	while (j < ft_count_words(s, c))
-	{
-		while (s[i] == c && s[i])
-			i++;
-		result[j] = (char *)malloc((ft_get_wlen(&s[i], c) + 1) * sizeof(char));
-		if (!result[j])
+		while (j - 1 >= 0)
 		{
-			ft_free_mem(result, j - 1);
-			return (NULL);
+			free(result[j - 1]);
+			j--;
 		}
-		k = 0;
-		while (s[i] != c && s[i])
-			result[j][k++] = s[i++];
-		result[j++][k] = '\0';
+		free(*result);
+		return (1);
 	}
-	result[j] = NULL;
+	return (0);
 }
 
 char	**ft_split(const char *s, char c)
@@ -82,7 +60,7 @@ char	**ft_split(const char *s, char c)
 	int		j;
 	int		k;
 
-	result = (char **)malloc((ft_count_words(s, c) + 1) * sizeof(char *));
+	result = ft_calloc(ft_count_words(s, c) + 1, sizeof(char *));
 	if (!result)
 		return (NULL);
 	i = 0;
@@ -91,12 +69,9 @@ char	**ft_split(const char *s, char c)
 	{
 		while (s[i] == c && s[i])
 			i++;
-		result[j] = (char *)malloc((ft_get_wlen(&s[i], c) + 1) * sizeof(char));
-		if (!result[j])
-		{
-			ft_free_mem(result, j - 1);
+		result[j] = malloc((ft_get_wlen(&s[i], c) + 1) * sizeof(char));
+		if (ft_free_mem(result, j))
 			return (NULL);
-		}
 		k = 0;
 		while (s[i] != c && s[i])
 			result[j][k++] = s[i++];
@@ -105,3 +80,27 @@ char	**ft_split(const char *s, char c)
 	result[j] = NULL;
 	return (result);
 }
+
+// #include <stdio.h>
+// int main() {
+//     const char *input = "This is a test string";
+//     char **result = ft_split(input, ' ');
+// 	system("leaks a.out");
+
+//     if (!result) {
+//         printf("Memory allocation error in ft_split\n");
+//         return 1;
+//     }
+
+//     // Print the split strings
+//     for (int i = 0; result[i] != NULL; i++) {
+//         printf("Token %d: %s\n", i, result[i]);
+//     }
+
+//     // Free the memory allocated by ft_split
+//     for (int i = 0; result[i] != NULL; i++) {
+//         free(result[i]);
+//     }
+//     free(result);
+//     return 0;
+// }
