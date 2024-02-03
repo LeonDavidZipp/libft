d1eb8bffc7febc83de1d7615954cae99f633200f
Leon David Zipp <leondavidzipp@gmx.de>
2024-02-03 23:41:23 +0100

    Commit message

--- ft_strsepjoin.c
+++ ft_strsepjoin.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:32:48 by lzipp             #+#    #+#             */
-/*   Updated: 2023/10/30 23:24:33 by lzipp            ###   ########.fr       */
+/*   Updated: 2023/11/01 02:10:18 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,14 @@ char	*ft_strsepjoin(char const *s1, char const *s2, char const *sep)
 	result = (char *)ft_calloc((len1 + seplen + len2 + 1), sizeof(char));
 	if (!result)
 		return (NULL);
-	len1 = 0;
-	while (s1[len1++])
-		result[len1 - 1] = s1[len1 - 1];
-	seplen = 0;
-	while (sep[seplen++])
-		result[len1 + seplen - 1] = sep[seplen - 1];
-	len2 = 0;
-	while (s2[len2++])
-		result[len1 + seplen + len2 - 1] = s2[len2 - 1];
+	len1 = -1;
+	while (s1[++len1])
+		result[len1] = s1[len1];
+	seplen = -1;
+	while (sep[++seplen])
+		result[len1 + seplen] = sep[seplen];
+	len2 = -1;
+	while (s2[++len2])
+		result[len1 + seplen + len2] = s2[len2];
 	return (result);
 }
