a8a4e2de0ea039c028aedcf02f0fecb686259c38
LeondavidZipp <leondavidzipp@gmx.de>
2024-02-12 16:12:13 +0100

    fixed errors in free_null_term_arrs

--- src/ft_free_null_term_arrs.c
+++ src/ft_free_null_term_arrs.c
@@ -3,12 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_free_null_term_arrs.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lzipp <lzipp@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 00:23:00 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/04 00:33:31 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/02/12 16:11:35 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../libft.h"
 
 void	ft_free_2d_arr(void **arr)
 {
@@ -16,8 +18,8 @@ void	ft_free_2d_arr(void **arr)
 
 	i = -1;
 	while (arr[++i])
-		ft_safe_free(arr[i]);
-	ft_safe_free(arr);
+		free(arr[i]);
+	free(arr);
 }
 
 void	ft_free_3d_arr(void ***arr)
@@ -27,7 +29,7 @@ void	ft_free_3d_arr(void ***arr)
 	i = -1;
 	while (arr[++i])
 		ft_free_2d_arr(arr[i]);
-	ft_safe_free(arr);
+	free(arr);
 }
 
 void	ft_free_4d_arr(void ****arr)
@@ -37,5 +39,5 @@ void	ft_free_4d_arr(void ****arr)
 	i = -1;
 	while (arr[++i])
 		ft_free_3d_arr(arr[i]);
-	ft_safe_free(arr);
+	free(arr);
 }
