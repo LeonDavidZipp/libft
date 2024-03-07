2d5ad5c427e68362d947639a20d757c7a09ae63f
LeondavidZipp <leondavidzipp@gmx.de>
2024-03-07 15:05:56 +0100

    added strntrim and ...in_place, fow trimming n of the chars in set

--- src/ft_strtrim.c
+++ src/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:33:36 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/23 20:30:29 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/03/07 15:05:09 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,34 @@ char	*ft_strtrim(char const *s1, char const *set)
 	return (res);
 }
 
+char	*ft_strntrim(char const *s1, char const *set, int n)
+{
+	int		i;
+	int		front_i;
+	int		rev_i;
+	int		front_n;
+	char	*res;
+
+	if (!s1 || !set)
+		return (NULL);
+	if (ft_strlen(s1) == 0)
+		return ((char *)s1);
+	front_i = 0;
+	rev_i = ft_strlen(s1) - 1;
+	front_n = n;
+	while (s1[front_i] && in_set(s1[front_i], set) && front_n--)
+		front_i++;
+	while (rev_i > 0 && s1[rev_i] && in_set(s1[rev_i], set), n--)
+		rev_i--;
+	res = (char *)ft_calloc(rev_i + 2 - front_i, sizeof(char));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (front_i <= rev_i)
+		res[i++] = s1[front_i++];
+	return (res);
+}
+
 char	*ft_trim_in_place(char const *s1, char const *set)
 {
 	char	*res;
@@ -63,5 +91,19 @@ char	*ft_trim_in_place(char const *s1, char const *set)
 		return (NULL);
 	res = ft_strdup(temp);
 	free(temp);
+	return (res);
+}
+
+char	*ft_strntrim_in_place(char *s1, char const *set, int n)
+{
+	char	*res;
+	char	*temp;
+
+	temp = ft_strntrim(s1, set, n);
+	if (!temp)
+		return (NULL);
+	res = ft_strdup(temp);
+	free(temp);
+	free(s1);
 	return (res);
 }
