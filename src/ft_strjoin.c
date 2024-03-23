3be15a131e8c16dae4b865bdcee0fbd15b160650
LeondavidZipp <leondavidzipp@gmx.de>
2024-03-23 18:33:23 +0100

    adjusted strjoin

--- src/ft_strjoin.c
+++ src/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:32:48 by lzipp             #+#    #+#             */
-/*   Updated: 2024/03/22 18:22:27 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/03/23 18:32:59 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,26 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*result;
-	size_t	len1;
-	size_t	len2;
+	int		len1;
+	int		len2;
 
+	if (!s1)
+		s1 = ft_strdup("");
+	if (!s2)
+		s2 = ft_strdup("");
 	if (!s1 || !s2)
 		return (NULL);
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
-	result = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+	result = (char *)ft_calloc(len1 + len2 + 1, sizeof(char));
 	if (!result)
 		return (NULL);
-	len1 = 0;
-	while (s1[len1])
-	{
+	len1 = -1;
+	while (s1[++len1])
 		result[len1] = s1[len1];
-		len1++;
-	}
-	len2 = 0;
-	while (s2[len2])
-	{
+	len2 = -1;
+	while (s2[++len2])
 		result[len1 + len2] = s2[len2];
-		len2++;
-	}
-	result[len1 + len2] = '\0';
 	return (result);
 }
 
@@ -45,10 +42,6 @@ char	*ft_join_in_place(char *s1, char *s2)
 {
 	char	*temp;
 
-	if (!s1)
-		s1 = ft_strdup("");
-	if (!s2)
-		s2 = ft_strdup("");
 	temp = ft_strjoin(s1, s2);
 	if (!temp)
 		return (NULL);
