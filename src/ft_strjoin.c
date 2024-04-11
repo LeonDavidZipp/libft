b9251e5e8a575b1f3d3786e6a157ca6092ec07e5
LeondavidZipp <leondavidzipp@gmx.de>
2024-04-11 11:43:45 +0200

    fixed ft_strjoin

--- src/ft_strjoin.c
+++ src/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:32:48 by lzipp             #+#    #+#             */
-/*   Updated: 2024/04/06 15:20:21 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/04/11 11:43:31 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		len1;
 	int		len2;
 
-	if (!s1)
-		s1 = ft_strdup("");
-	if (!s2)
-		s2 = ft_strdup("");
-	if (!s1 || !s2)
-		return (NULL);
+	if (!s1 && s2)
+		return (ft_strdup(s2));
+	if (!s2 && s1)
+		return (ft_strdup(s1));
+	if (!s1 && !s2)
+		return (ft_strdup(""));
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
 	result = (char *)ft_calloc(len1 + len2 + 1, sizeof(char));
